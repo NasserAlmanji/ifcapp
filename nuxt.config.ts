0e81f0da394aaf560e14a4f487fef67f10630230
Chawki.Ferroukhi <75191392+ChawkiFerroukhi@users.noreply.github.com>
2025-03-02 03:16:53 +0100

    fix: fix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: false },
   publicRuntimeConfig: {
-    API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8090/api",
+    API_BASE_URL: process.env.API_BASE_URL,
   },
 
   css: ["~/assets/css/global.css"],
